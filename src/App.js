d17204b2bebe5e755bad61f78eadc0c4b9f7ebd5
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2020-12-13 16:42:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     super(props);
     this.state = {
       countRight: 0,
-      countWrong: 0
+      countWrong: 0,
     };
   }
 
@@ -49,40 +49,3 @@ export default class App extends Component {
     );
   }
 }
-
-/*<div className="flashcard-list">
-          {flashCards.map((cards)) => {
-            return (
-              <Card
-              frontSide={cards.frontSide}
-              backSide={cards.backSide}
-              handleClick={this.handleClick}
-              handleNoClick={this.handleNoClick}
-              )
-          }}
-        </div>
-        
-        <Card
-          handleClick={this.handleClick}
-          handleNoClick={this.handleNoClick}
-          frontSide="What is a smoke test?"
-          backSide="When applied to web applications, smoke tests verify that the most important functionality is working. By design, smoke tests do not cover every permutation and edge case. They instead check that your application isn't so broken that additional testing would be a waste of time."
-        />
-        <Card
-          handleClick={this.handleClick}
-          handleNoClick={this.handleNoClick}
-          frontSide="What does the exact prop of the Route component do?"
-          backSide="The exact prop specifies that the route has to exactly match in order for the component to display. If the Route component didn't have the exact prop, the component would display for exact matches and partial matches."
-        />
-        <Card
-          handleClick={this.handleClick}
-          handleNoClick={this.handleNoClick}
-          frontSide="Explain the difference between props, state, and Context."
-          backSide="State - This is data maintained inside a component. It is local or owned by that specific component. The component itself will update the state using the setState function.
-Props - Data passed in from a parent component. props are read-only in the child component that receives them. However, callback functions can also be passed, which can be executed inside the child to initiate an update.
-The difference is all about which component owns the data. State is owned locally and updated by the component itself. Props are owned by a parent component and are read-only. Props can only be updated if a callback function is passed to the child to trigger an upstream change.
-The state of a parent component can be passed a prop to the child. They are referencing the same value, but only the parent component can update it.
-Context is designed to share data that can be considered “global” for a tree of React components, such as the current authenticated user, theme, or preferred language. 
- 
-"
-        />*/
